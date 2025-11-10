a86c961c5df4109dca289fe784071d78030e0245
zhuangxing <277308275@qq.com>
2025-11-10 08:40:09 +0000

    clean utils.py

--- maze/utils/utils.py
+++ maze/utils/utils.py
@@ -3,13 +3,8 @@ import subprocess
 
 def collect_gpu_info():
         try:
-            # 执行 nvidia-smi 命令并捕获输出
-            result = subprocess.run(['nvidia-smi', '--query-gpu=index,name,utilization.gpu,memory.total,memory.used,memory.free', '--format=csv,noheader,nounits'], stdout=subprocess.PIPE)
-            
-            # 解码输出为字符串
+            result = subprocess.run(['nvidia-smi', '--query-gpu=index,name,utilization.gpu,memory.total,memory.used,memory.free', '--format=csv,noheader,nounits'], stdout=subprocess.PIPE)   
             output = result.stdout.decode('utf-8')
-            
-            # 按行分割输出
             lines = output.strip().split('\n')
             
             info = []
@@ -38,13 +33,7 @@ def collect_gpu_info():
 
 def get_available_ports(n=2):
     """
-    获取当前机器上 n 个可用的 TCP 端口。
-    
-    参数:
-        n (int): 需要获取的端口数量，默认为2。
-    
-    返回:
-        List[int]: 包含 n 个可用端口号的列表。
+    Get a list of n available ports on the current machine.
     """
     ports = []
     sockets = []
@@ -52,12 +41,11 @@ def get_available_ports(n=2):
     try:
         for _ in range(n):
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.bind(('', 0))  # 绑定到任意地址，端口由系统自动分配
+            sock.bind(('', 0))  # Bind to any address, port automatically assigned by the system
             port = sock.getsockname()[1]
             ports.append(port)
-            sockets.append(sock)  # 保持 socket 打开，防止端口被立即复用
+            sockets.append(sock) 
         return ports
     finally:
-        # 关闭所有打开的 sockets
         for sock in sockets:
             sock.close()
